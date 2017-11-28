98d4f9b004420f3704989d59cf4e9e708792a11e
Unknown <trond_eriks@hotmail.com>
2017-11-28 08:31:45 +0100

    Added sorting on ApplicationList

--- DynamicApp/DynamicApp/Controllers/ApplicationsController.cs
+++ DynamicApp/DynamicApp/Controllers/ApplicationsController.cs
@@ -21,18 +21,63 @@ namespace DynamicApp.Controllers
             return View(db.CMDynamicApplications.ToList());
         }
 
-        public ActionResult ApplicationList(int customerID)
+        public ActionResult ApplicationList(string sortOrder, int customerID)
         {
 
             var customerApplications = new CustomerApplicationsVM();
-            
+
+
+            ViewBag.ApplicationNameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.ApplicationGroupSortParm = sortOrder == "Index" ? "index_desc" : "Index";
+            ViewBag.ApplicationVersionSortParm = sortOrder == "Version" ? "version_desc" : "Version";
+            ViewBag.ApplicationVendorSortParm = sortOrder == "Vendor" ? "vendor_desc" : "Vendor";
+            ViewBag.ApplicationCreatedBySortParm = sortOrder == "CreatedBy" ? "createdby_desc" : "CreatedBy";
+            ViewBag.ApplicationDateCreatedSortParm = sortOrder == "DateCreated" ? "datecreated_desc" : "DateCreated";
+            ViewBag.ApplicationModifiedBySortParm = sortOrder == "LastModifiedBy" ? "modifiedby_desc" : "ModifiedBy";
+            ViewBag.ApplicationDateLastModifiedSortParm = sortOrder == "DateLastModified" ? "datelastmodified_desc" : "DateLastModified";
+
+
             var customerApps = db.DynamicAppCustomers.Where(c => c.CustomerID == customerID && c.ApplicationID != null)
-                                                       .Select(c=> c.CMDynamicApplication.id)
-                                                        .ToList();
-            var appsCustomerDoesntHave = db.CMDynamicApplications.Where(d => !customerApps.Contains(d.id)).ToList();
+                                                       .Select(c => c.CMDynamicApplication.id)
+                                                       .ToList();
+
+            // var appsCustomerDoesntHave = db.CMDynamicApplications.Where(d => !customerApps.Contains(d.id)).OrderBy(a => a.Name);
             Customer customer = db.Customers.Where(c => c.id == customerID).FirstOrDefault();
             customerApplications.Customer = customer;
-            customerApplications.ApplicationList = appsCustomerDoesntHave;
+            var appsCustomerDoesntHave = from a in db.CMDynamicApplications where !customerApps.Contains(a.id) select a;
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    appsCustomerDoesntHave = appsCustomerDoesntHave.OrderByDescending(a => a.Name);
+                    break;
+                case "Index":
+                    appsCustomerDoesntHave = appsCustomerDoesntHave.OrderBy(a => a.Index);
+                    break;
+                case "Version":
+                    appsCustomerDoesntHave = appsCustomerDoesntHave.OrderBy(a => a.Version);
+                    break;
+                case "Vendor":
+                    appsCustomerDoesntHave = appsCustomerDoesntHave.OrderBy(a => a.Vendor);
+                    break;
+                case "CreatedBy":
+                    appsCustomerDoesntHave = appsCustomerDoesntHave.OrderBy(a => a.CreatedBy);
+                    break;
+                case "DateCreated":
+                    appsCustomerDoesntHave = appsCustomerDoesntHave.OrderBy(a => a.DateCreated);
+                    break;
+                case "LastModifiedBy":
+                    appsCustomerDoesntHave = appsCustomerDoesntHave.OrderBy(a => a.LastModifiedBy);
+                    break;
+                case "DateLastModified":
+                    appsCustomerDoesntHave = appsCustomerDoesntHave.OrderBy(a => a.DateLastModified);
+                    break;
+                default:
+                    appsCustomerDoesntHave = appsCustomerDoesntHave.OrderBy(a => a.Name);
+                    break;
+            }
+            customerApplications.ApplicationList = appsCustomerDoesntHave.ToList();
+
             return View(customerApplications);
         }
 
